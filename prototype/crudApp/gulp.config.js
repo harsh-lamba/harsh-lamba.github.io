3f125f66e4f12b76bae24a16ef7239a3db95a41e
harsh-lamba <lharsh18@yahoo.in>
2016-08-04 12:48:45 +0530

    copyied startup application and gulp task

---
+++ prototype/crudApp/gulp.config.js
@@ -0,0 +1,31 @@
+/* jshint node: true, -W024, -W069 */
+
+'use strict';
+
+module.exports = function() {
+	
+	var root 		= "./";
+	var app 		= root + "app/";
+	var bowerFiles 	= root + "bower_components/"
+	var assets 		= 'app/assets/';
+
+	var config 		= {
+		'rootSass'	: assets + 'styles/app.scss',
+		'allSass'	: assets + 'styles/**/*.scss',
+		'allLib' 	: [
+						//bowerFiles + "jquery/dist/jquery.min.js",
+						bowerFiles + "bootstrap-sass/assets/javascripts/bootstrap.min.js",
+						bowerFiles + "angular/angular.min.js"
+					  ],
+		"allApp"	: [
+						app + "**/*.module.js",
+						app + "**/*.config.js",
+						app + "**/*.constant.js",
+						app + "**/*.controller.js",
+						app + "**/*.service.js"
+					  ],
+		"temp"		: app + "tmp"
+	};
+
+	return config;
+}
